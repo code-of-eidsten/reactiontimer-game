345ea7da552bb3c00fe4d6019acfc1a5951a351f
waninko <linn.eidsten@gmail.com>
2026-01-15 14:07:59 +0100

    results in its own component, feedback on clickspeed

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,39 @@
 <template>
   <h1>Reaction Timer Game</h1>
-  <h2>{{ score !== null ? `Your Reaction Time: ${score} ms!` : '' }}</h2>
+  <Results v-if="showResults" :score="score" />
   <button @click="start" :disabled="isPlaying">Start Game</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" />
 </template>
 
 <script>
-import Block from './components/Block.vue';
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
   components: {
-    Block
+    Block,
+    Results,
   },
   data() {
     return {
       isPlaying: false,
       delay: null,
       score: null,
+      showResults: false,
     }
   },
   methods: {
     start() {
-      this.delay = Math.floor(Math.random() * 5000) + 2000;
-      this.isPlaying = true;
+      this.delay = Math.floor(Math.random() * 5000) + 2000
+      this.isPlaying = true
+      this.showResults = false
+      this.score = null
     },
     endGame(reactionTime) {
-      this.score = reactionTime;
-      this.isPlaying = false;
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
     }
   },
 };
---
+++ src/components/Results.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="results">
+        <h2> Your Reaction Time: {{ score }} ms!</h2>
+        <h1>{{ rank }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if (this.score < 300) {
+            this.rank = '⚡Lightning has nothing on you!⚡';
+        } else if (this.score < 400) {
+            this.rank = ' 👏Pro clicker!👏';
+        } else if (this.score < 600) {
+            this.rank = 'Average reaction 🙆';
+        } else {
+            this.rank = 'Sloooow..🐌';
+        }
+    }
+}
+</script>
+
+<style>
+.results {
+    margin: 20px auto;
+    padding: 20px;
+    border: 2px solid rgb(100, 117, 230);
+    border-radius: 10px;
+    width: 450px;
+    background-color: #f9f9f9;
+}
+</style>
