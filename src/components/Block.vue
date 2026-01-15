2ca29a20b34ee9640a2d0aba556158d23e6b2d29
waninko <linn.eidsten@gmail.com>
2026-01-15 13:25:11 +0100

    lifecycle refresher + timer

---
+++ src/components/Block.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="block" v-if="showBlock"> CLICK ME FAST!</div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            clicked: false,
+        }
+    },
+    mounted() {
+        console.log(`Block component mounted.`);
+        setTimeout(() => {
+            this.showBlock = true;
+            console.log(`Current delay: ${this.delay} ms.`);
+        }, this.delay);
+    },
+    updated() {
+        console.log(`Block component updated.`);
+    },
+    unmounted() {
+        console.log(`Block component unmounted.`);
+    },
+
+}
+</script>
+
+<style>
+.block {
+    width: 200px;
+    border-radius: 20px;
+    background-color: rgb(100, 117, 230);
+    color: white;
+    font-size: 20px;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+    cursor: pointer;
+}
+</style>
